a23c34581095f7a7b5f6413c3aeb21de40a4ebb9
Ravi-Rsankar <ravisankar.r93@gmail.com>
2023-05-16 10:52:38 +0530

    font awesome integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/js/bootstrap';
+import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
+import './assets/fontawesome/css/all.css'
 import WidgetContainer from './pages/Widgets';
 
 function App() {
